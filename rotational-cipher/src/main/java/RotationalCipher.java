0bbcfae1eb7c49a796e4b7e443886ae979679379
Tim Austin <tim@neenjaw.com>
2019-05-24 01:30:59 -0600

    rot soln

--- rotational-cipher/src/main/java/RotationalCipher.java
+++ rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,47 @@
 class RotationalCipher {
+    private final int CIPHER_SHIFT_KEY;
+    private final int ALPHABET_LENGTH = 26;
+
+    private final int UPPER_CASE_RANGE_START = (int) 'A';
+    private final int UPPER_CASE_RANGE_END = (int) 'Z';
+
+    private final int LOWER_CASE_RANGE_START = (int) 'a';
+    private final int LOWER_CASE_RANGE_END = (int) 'z';
 
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        CIPHER_SHIFT_KEY = shiftKey;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return data.codePoints()
+                   .map(c -> rotateCodePoint(c))
+                   .collect(StringBuilder::new,
+                            StringBuilder::appendCodePoint,
+                            StringBuilder::append)
+                   .toString();
     }
 
+    int rotateCodePoint(int c) {
+        if (inUpperCaseRange(c)) {
+            return shiftCodePoint(c, UPPER_CASE_RANGE_START);
+        } 
+        else if (inLowerCaseRange(c)) {
+            return shiftCodePoint(c, LOWER_CASE_RANGE_START);
+        }
+        else {
+            return c;
+        }
+    }
+
+    boolean inUpperCaseRange(int c) {
+        return (UPPER_CASE_RANGE_START <= c) && (c <= UPPER_CASE_RANGE_END);
+    }
+
+    boolean inLowerCaseRange(int c) {
+        return (LOWER_CASE_RANGE_START <= c) && (c <= LOWER_CASE_RANGE_END);
+    }
+
+    int shiftCodePoint(int c, int rangeStart) {
+        return (((c - rangeStart) + CIPHER_SHIFT_KEY) % ALPHABET_LENGTH) + rangeStart;
+    }
 }
